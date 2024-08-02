f2c13d0c6248b142c6457b1b962fab785f213d09
AlexBeje <alex.bejenaru93@gmail.com>
2024-08-02 15:05:03 +0200

    📦 NEW: Added localhost to origin cors.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://www.recomotor.alexbeje.dev'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
   await app.listen(3001);
 }
 bootstrap();

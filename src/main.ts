5aa4a3ec9f5fa3cad3731f2e1bee778ef59d33da
AlexBeje <alex.bejenaru93@gmail.com>
2024-08-02 15:46:45 +0200

    📦 NEW: Added vercel methods in settings.

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:3000',
-      'http://localhost:3000/',
+ 
       'https://recomotor-back.alexbeje.dev',
       'https://recomotor-back.alexbeje.dev/',
       'https://recomotor-back.alexbeje.dev/cars',

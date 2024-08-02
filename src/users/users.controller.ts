246b7a20507545e4c3904d77943dfe7377d88013
AlexBeje <alex.bejenaru93@gmail.com>
2024-08-02 16:04:39 +0200

    📦 NEW: Added localhost origin to the cors.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -31,20 +31,20 @@ export class UsersController {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new HttpException('Invalid id', 400);
     // check if the provided favorites array has the correct structure
-    if (
-      !Array.isArray(updateUserDto.favorites) ||
-      updateUserDto.favorites.some(
-        (favorite) =>
-          typeof favorite.id !== 'number' ||
-          typeof favorite.brand !== 'string' ||
-          typeof favorite.model !== 'string',
-      )
-    ) {
-      throw new HttpException(
-        'Favorites should be an array of objects with id, brand and model',
-        400,
-      );
-    }
+    // if (
+    //   !Array.isArray(updateUserDto.favorites) ||
+    //   updateUserDto.favorites.some(
+    //     (favorite) =>
+    //       typeof favorite.id !== 'number' ||
+    //       typeof favorite.brand !== 'string' ||
+    //       typeof favorite.model !== 'string',
+    //   )
+    // ) {
+    //   throw new HttpException(
+    //     'Favorites should be an array of objects with id, brand and model',
+    //     400,
+    //   );
+    // }
     return this.userService.updateUser(id, updateUserDto);
   }
 }
